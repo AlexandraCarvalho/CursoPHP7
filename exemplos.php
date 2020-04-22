1fca9354c6f0ba25018efe93c576fe8d6f02d8ae
Alexandra <alexandracarvalho0202@gmail.com>
2020-04-22 16:31:50 -0300

    adicionando um arquivo só para todos os exeplos apresentados

---
+++ exemplos.php
@@ -0,0 +1,26 @@
+<?php
+/*exdemplo-01*/
+
+//declaração de variável
+$nome = "Alexandra";
+
+echo $nome;
+//dando espaço entre a listagem dos itens
+echo "<br/>";
+//descrição da variável, listando tipo e quantidade de caracteres
+var_dump($nome);
+
+
+/*exemplo-02*/
+
+//padrão camelCase
+$anoNascimento = 1995;
+
+//caracter especial permitida
+$nome_completo = "Alexandra Carvalho";
+
+//limpando a variavel -pode ser passado mais de uma variável
+unset($anoNascimento);
+
+echo $anoNascimento;//chamando novamente a variável que foi limpa
+?>
